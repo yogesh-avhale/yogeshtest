a7443434c981dd860a82c94e0f911eac5bb93338
yogesh-avhale <46989330+yogesh-avhale@users.noreply.github.com>
2020-09-20 14:49:12 +0530

    Update user_form.php

--- application/views/user_form.php
+++ application/views/user_form.php
@@ -28,7 +28,7 @@
 	    <div class="modal-content">
 	      <div class="modal-header">
 	        <button type="button" class="close" data-dismiss="modal">&times;</button>
-	        <h4 class="modal-title"> User form</h4>
+	        <h4 class="modal-title">Git User form</h4>
 	      </div>
 	      	<div class="modal-body">
 	      		<form action="<?php echo base_url();?>save/userinfo" class="form" id="user_data">
